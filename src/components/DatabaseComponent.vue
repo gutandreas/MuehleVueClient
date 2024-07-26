5f07d122ef913254c8596388e23e64a8e9023940
Andreas Gut <andreasgut@gmx.ch>
2024-07-26 16:24:47 +0200

    DatabaseComponent.vue hinzugefügt

--- src/components/DatabaseComponent.vue
+++ src/components/DatabaseComponent.vue
@@ -1,15 +1,44 @@
-<script setup>
+<script>
 
 export default {
   name: 'DatabaseComponent',
-  computed: {
+  props: {
+    sendMessage: {
+      type: Function,
+      required: true,
+    }
 
+  },
+  methods: {
+    getAllGames(){
+      const json = JSON.stringify({
+        "category": "database",
+        "command": "get",
+        "elements": "games"
+
+      })
+      console.log(json)
+      this.sendMessage(json);
+    }
   }
 }
 
 </script>
 
 <template>
+  <div class="database-container">
+    <div class="card m-1 next-step-card">
+      <div class="card-header text-center">
+        <h5>Datenbank</h5>
+      </div>
+      <div class="card-body">
+        <button  @click="getAllGames">Alle Games abfragen</button>
+        <button>Alle laufenden Games abfragen</button>
+      </div>
+    </div>
+  </div>
+
+
 
 </template>
 
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,27 +1,73 @@
-<script>
-export default {
-  name: 'AdminDashboard'
-};
-</script>
-
 <template>
-  <div>
-
-    <h1>Admin Dashboard</h1>
-    <!-- Admin dashboard content -->
+  <div class="grid-container">
+    <DatabaseComponent class="center-panel" :sendMessage="sendMessage" :messages="messages" />
   </div>
 </template>
 
 
+<script>
+
+import DatabaseComponent from "@/components/DatabaseComponent.vue";
+
+export default {
+  name: 'AdminDashboard',
+  components: {DatabaseComponent},
+  data() {
+    return {
+      messages: [], // Messages received from the WebSocket server
+    };
+  },
+  mounted() {
+    this.$initWebSocket(this.$adminWebsocketUrl);
+  },
+  beforeUnmount() {
+    this.$closeWebSocket(this.$adminWebsocketUrl);
+  },
+  methods: {
+    handleWebSocketMessage(message) {
+      const data = JSON.parse(message);
+      this.messages.push(data); // Beispiel für die Verarbeitung der empfangenen Nachricht
+    },
+    sendMessage(message) {
+      this.$sendMessage(this.$adminWebsocketUrl, message);
+    }
+  }
+};
+</script>
+
+
+
+
 
 <style scoped>
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 0.5fr 1fr 0.5fr; /* Setzt die Breiten der Spalten */
+  grid-template-rows: auto; /* Auto Höhe basierend auf dem Inhalt */
+  grid-template-areas:
+    "left center right";
+  width: 100%;
+}
+
+.left-panel {
+  grid-area: left;
+  width: 100%;
+  border-right: 2px solid #fff; /* Optional: Border zur Trennung */
+}
+
 .center-panel {
-  margin: 3%;
   grid-area: center;
   align-items: center; /* Zentriert das Spielfeld vertikal */
   justify-content: center; /* Zentriert das Spielfeld horizontal */
   width: 100%;
   height: 100%;
   border-top: 2px solid #fff; /* Optional: Border zur Trennung */
+}
+
+.right-panel {
+  grid-area: right;
+  width: 100%;
+  border-left: 2px solid #fff; /* Optional: Border zur Trennung */
 }
 </style>
