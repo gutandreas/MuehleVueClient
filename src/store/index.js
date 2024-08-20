823ad3d35960ca88e48918b95efda82df073eb70
Andreas Gut <andreasgut@gmx.ch>
2024-08-20 23:52:04 +0200

    Setups erweitert

--- src/store/index.js
+++ src/store/index.js
@@ -12,17 +12,23 @@ const consoleLogger = (store) => {
 const store = createStore({
     state: {
         uuid: null,
-        name: null,
+        player1Name: null,
+        player2Name: null,
         stonecolor: null,
         firststone: null,
-        index: null
+        index: null,
+        board: null
     },
     mutations: {
         setUuid(state, payload){
             state.uuid = payload.uuid;
+            console.log(state.uuid)
         },
-        setName(state, payload){
-            state.name = payload.name;
+        setPlayer1Name(state, payload){
+            state.player1Name = payload.player1Name;
+        },
+        setPlayer2Name(state, payload){
+            state.player2Name = payload.player2Name;
         },
         setColor(state, payload){
             state.stonecolor = payload.stonecolor;
@@ -32,50 +38,41 @@ const store = createStore({
         },
         setIndex(state, payload){
             state.index = payload.index;
+        },
+        setBoard(state, payload){
+            state.board = payload.board;
         }
-
     },
     actions: {
+        subscribeToServer(context) {
+            stompService.subscribe('/user/queue/reply', (response) => {
+                const data = JSON.parse(response.body);
+                console.log("Response received: ", data);
+                context.commit("setUuid", data.uuid);
+                context.commit("setPlayer1Name", data.player1Name);
+                context.commit("setPlayer2Name", data.player2Name);
+                context.commit("setColor", data.stonecolor);
+                context.commit("setIndex", data.index);
+            });
+        },
         setupComputerGame(context, payload){
+            context.dispatch('subscribeToServer'); // Subscription wird einmal zentral aufgerufen
             const setupComputerGameDO = {
                 name: payload.name,
                 level: payload.level,
                 stonecolor: payload.stonecolor,
                 firststone: payload.firststone,
             }
-
-            stompService.subscribe('/user/queue/reply', (response) => {
-                // Hier die Antwort vom Server verarbeiten
-                const data = JSON.parse(response.body);
-                console.log("Response received: ", data);
-                context.commit("setUuid", data.uuid);
-                context.commit("setName", data.name);
-                context.commit("setColor", data.stonecolor);
-                context.commit("setIndex", data.index)
-            });
-
             stompService.send('/manager/setup/computer', setupComputerGameDO)
         },
         setupLoginGameStart(context, payload){
-
-
-            stompService.subscribe('/user/queue/reply', (response) => {
-                // Hier die Antwort vom Server verarbeiten
-                const data = JSON.parse(response.body);
-                console.log("Response received: ", data);
-                context.commit("setUuid", data.uuid);
-                context.commit("setName", data.name);
-                context.commit("setColor", data.stonecolor);
-                context.commit("setIndex", data.index)
-            });
-
+            context.dispatch('subscribeToServer'); // Subscription wird einmal zentral aufgerufen
             stompService.send('/manager/setup/start', payload)
-
-
         }
     },
     getters: {},
     plugins: [consoleLogger]
 })
+
 
 export default store;
